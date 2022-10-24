5a21c6db32ba33fb2af9c28539d9c944e6b78e77
Ronald Pardo <r.diazp@uniandes.edu.co>
2022-10-23 19:47:37 -0500

    Typescript workshop #1 ~ ISIS 2603

---
+++ Data/data.js
@@ -0,0 +1,52 @@
+"use strict";
+// Series data
+var _a;
+class Serie {
+    constructor(id, name, channel, seasons, description, link, banner) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.description = description;
+        this.link = link;
+        this.banner = banner;
+    }
+}
+const series = [
+    new Serie(1, "Breaking Bad", "AMC", 5, "Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer", "https://www.amc.com/shows/breaking-bad", "https://i.imgur.com/GGje0vc.jpg"),
+    new Serie(2, "Orange Is the New Black", "Netflix", 6, "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary", "https://www.netflix.com/co/title/70242311", "https://i.imgur.com/EvKe48G.jpg"),
+    new Serie(3, "Game of Thrones", "HBO", 7, "American fantasy drama", "https://www.hbo.com/game-of-thrones", "https://i.imgur.com/TDCEV1S.jpg"),
+    new Serie(4, "The Big Bang Theory", "CBS", 12, "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny, who aims to teach them a thing or two about life. Despite their on-again, off-again relationship in the past, Leonard and Penny have finally gotten married. Even Sheldon has found a female companion, entering into a relationship agreement with neurobiologist Amy Farrah Fowler, and he recently took their relationship to the next level by marrying her after a long courtship. In their free time, Leonard and Sheldon enjoy fantasy role-playing games with their ever-expanding universe of friends, including fellow scientists Koothrappali, Wolowitz, and Wolowitz’s adorable microbiologist wife, Bernadette, who is adjusting to life with their two children.", "https://www.cbs.com/shows/big_bang_theory/about/", "https://i.imgur.com/uAEpVWk.jpg"),
+    new Serie(5, "Sherlock", "BBC", 4, "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman), who has returned from military service in Afghanistan with the Royal Army Medical Corps", "https://www.bbc.co.uk/programmes/b018ttws", "https://i.imgur.com/02B7qhj.jpg"),
+    new Serie(6, "A Very English Scandal", "BBC", 2, "A Very English Scandal is a fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.", "https://www.bbc.co.uk/programmes/p065smy4", "https://i.imgur.com/D4y3DrQ.jpg"),
+];
+let average = 0;
+for (let i = 0; i < series.length; i++) {
+    average += series[i].seasons;
+}
+average = average / series.length;
+// Insert caption on table
+let captiontext = "Seasons average: " + average;
+let caption = document.createTextNode(captiontext);
+let captionelement = document.getElementById("tablecaption");
+captionelement === null || captionelement === void 0 ? void 0 : captionelement.appendChild(caption);
+// Add series to the DOM tbody
+let tbody = (_a = document.getElementById("seriestable")) === null || _a === void 0 ? void 0 : _a.querySelector("tbody");
+for (let i = 0; i < series.length; i++) {
+    let serie = series[i];
+    let row = document.createElement("tr");
+    let th = document.createElement("th");
+    th.setAttribute("scope", "row");
+    th.innerHTML = serie.id.toString();
+    row.appendChild(th);
+    let tdname = document.createElement("td");
+    tdname.innerHTML = serie.name;
+    row.appendChild(tdname);
+    let tdchannel = document.createElement("td");
+    tdchannel.innerHTML = serie.channel;
+    row.appendChild(tdchannel);
+    let tdseasons = document.createElement("td");
+    tdseasons.innerHTML = serie.seasons.toString();
+    row.appendChild(tdseasons);
+    tbody === null || tbody === void 0 ? void 0 : tbody.appendChild(row);
+}
