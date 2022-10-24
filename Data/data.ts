52900556b007a44c2f29c748360b6632ecfa43d4
Ronald Pardo <r.diazp@uniandes.edu.co>
2022-10-23 23:01:43 -0500

    Typescript workshop #2

--- Data/data.ts
+++ Data/data.ts
@@ -23,22 +23,23 @@ class Serie {
 
 const series = [
     new Serie (1,"Breaking Bad","AMC", 5,"Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer" ,
-    "https://www.amc.com/shows/breaking-bad","https://i.imgur.com/GGje0vc.jpg") ,
+    "https://www.amc.com/shows/breaking-bad","/Assets/Breaking-bad.jpg") ,
     
     new Serie (2,"Orange Is the New Black", "Netflix", 6, "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary", 
-    "https://www.netflix.com/co/title/70242311","https://i.imgur.com/EvKe48G.jpg"),
+    "https://www.netflix.com/co/title/70242311","/Assets/Orange-is-the-new-black.jpg"),
     
-    new Serie (3, "Game of Thrones","HBO", 7, "American fantasy drama", "https://www.hbo.com/game-of-thrones",
-      "https://i.imgur.com/TDCEV1S.jpg"),
+    new Serie (3, "Game of Thrones","HBO", 7, "In the mythical continent of Westeros, nine families of higher nobility (Targaryen, Lannisters, Starks, Tyrell, Martell, Greyjoys, Baratheons and Boltons) scramble bitterly to gain power over the seven kingdoms and the Iron throne.",
+        "https://www.hbo.com/game-of-thrones",
+        "/Assets/Game-of-thrones.jpg"),
 
-    new Serie (4, "The Big Bang Theory", "CBS", 12, "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny, who aims to teach them a thing or two about life. Despite their on-again, off-again relationship in the past, Leonard and Penny have finally gotten married. Even Sheldon has found a female companion, entering into a relationship agreement with neurobiologist Amy Farrah Fowler, and he recently took their relationship to the next level by marrying her after a long courtship. In their free time, Leonard and Sheldon enjoy fantasy role-playing games with their ever-expanding universe of friends, including fellow scientists Koothrappali, Wolowitz, and Wolowitz’s adorable microbiologist wife, Bernadette, who is adjusting to life with their two children.",
-        "https://www.cbs.com/shows/big_bang_theory/about/", "https://i.imgur.com/uAEpVWk.jpg"),
+    new Serie (4, "The Big Bang Theory", "CBS", 12, "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny, who aims to teach them a thing or two about life.",
+        "https://www.cbs.com/shows/big_bang_theory/about/", "/Assets/The-big-bang-theory.jpg"),
     
-    new Serie (5, "Sherlock", "BBC",  4, "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman), who has returned from military service in Afghanistan with the Royal Army Medical Corps",
-        "https://www.bbc.co.uk/programmes/b018ttws", "https://i.imgur.com/02B7qhj.jpg"),
+    new Serie (5, "Sherlock", "BBC",  4, "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman).",
+        "https://www.bbc.co.uk/programmes/b018ttws", "/Assets/Sherlock.jpg"),
 
     new Serie (6, "A Very English Scandal", "BBC", 2, "A Very English Scandal is a fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.",
-        "https://www.bbc.co.uk/programmes/p065smy4", "https://i.imgur.com/D4y3DrQ.jpg"),
+        "https://www.bbc.co.uk/programmes/p065smy4", "/Assets/A-very-english-scandal.jpg"),
 ];
 
 let average = 0;
@@ -66,7 +67,7 @@ for (let i=0; i<series.length; i++) {
     th.innerHTML = serie.id.toString();
     row.appendChild(th);
     let tdname = document.createElement("td");
-    tdname.innerHTML = serie.name;
+    tdname.innerHTML = createNameText(serie);
     row.appendChild(tdname);
     let tdchannel = document.createElement("td");
     tdchannel.innerHTML = serie.channel;
@@ -75,4 +76,50 @@ for (let i=0; i<series.length; i++) {
     tdseasons.innerHTML = serie.seasons.toString();
     row.appendChild(tdseasons);
     tbody?.appendChild(row);
+}
+// Card information and change
+
+let card = document.getElementById("cardblock");
+let cardimg = card?.querySelector("img"); // Will be used
+let cardbody = card?.querySelector(".card-body");
+let cardtitle = cardbody?.querySelector("#title"); // Will be used
+let cardtext = cardbody?.querySelector("#text"); // Will be used
+let cardlink = cardbody?.querySelector("#link"); // Will be used
+
+function setCardData(id: number) {
+    setCardEmptyData();
+    let serie = series[id-1];
+    cardimg?.setAttribute("src", serie.banner);
+    cardimg?.setAttribute("alt", serie.name);
+    let name = document.createTextNode(serie.name);
+    cardtitle?.appendChild(name);
+    let text = document.createTextNode(serie.description);
+    cardtext?.appendChild(text);
+    cardlink?.setAttribute("href", serie.link);
+}
+
+function setCardEmptyData() {
+    cardimg?.setAttribute("src", "");
+    cardimg?.setAttribute("alt", "");
+    if (cardtitle != null) {
+        cardtitle.innerHTML = "";
+    }
+    if (cardtext != null) {
+        cardtext.innerHTML = "";
+    }
+    cardlink?.setAttribute("href", "");
+}
+
+// Set card data to index 1 by default
+setCardData(1);
+
+// Function to create name with onclick action
+
+function createNameText(serie: Serie) {
+    let namebody = document.createElement("a");
+    namebody.setAttribute("href", "#");
+    namebody.setAttribute("onclick", "setCardData("+serie.id+")");
+    let nametext = document.createTextNode(serie.name);
+    namebody.appendChild(nametext);
+    return namebody.outerHTML;
 }
